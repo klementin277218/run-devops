37d6dbd3c77801d796befa408c8a27eda708d73d
klementin2022 <klementin.hasani@orbyta.com>
2022-07-17 16:24:25 +0200

    dockerfile

--- Shopping/Shopping.Client/Data/ProductContext.cs
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -8,7 +8,7 @@ namespace Shopping.Client.Data
         {
             new Product()
                 {
-                    Name = "IPhone X",
+                    Name = "IPhone 13 PRO MAX",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-1.png",
                     Price = 950.00M,
