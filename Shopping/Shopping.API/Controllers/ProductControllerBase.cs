f5e6fe6b3ce32a7f8c91ad62f2326bcbafde6fd2
klementin2022 <klementin.hasani@orbyta.com>
2022-08-09 22:00:14 +0200

    Before Mongo

---
+++ Shopping/Shopping.API/Controllers/ProductControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController, Route("[controller]")]
+    public class ProductControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductContext.Products;
+        }
+    }
+}
